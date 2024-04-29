f59111505a077a71c3acf528a8c68b9844b3dfcc
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2024-04-29 14:52:17 +0100

    feat: Updated utils.py

--- utils.py
+++ utils.py
@@ -1,14 +1,19 @@
-from re import search, IGNORECASE
+from re import search, IGNORECASE, Pattern, Match
 from discord import Embed
+from constants import Site
 from dateutil import tz
 from datetime import datetime
 
 now = datetime.now(tz=tz.tzlocal())
 
 
+def pattern_matcher(pattern: str | Pattern[str], string: str) -> Match[str] | None:
+    return search(pattern, string, IGNORECASE)
+
+
 def validate_url(url: str, url_patterns: list[str]) -> bool:
     for pattern in url_patterns:
-        if search(pattern, url, IGNORECASE):
+        if pattern_matcher(pattern, url):
             return True
     return False
 
@@ -34,36 +39,69 @@ def trim_text(text: str, max_length: int = 1024, suffix: str = " ...") -> str:
     return text[: max_length - len(suffix)] + suffix if len(text) > max_length else text
 
 
-def process_list(field_list: list[str]) -> str | None:
-    """Process a list of items and return a formatted string.
-
-    Args:
-        field_list: The list of items to process.
-
-    Returns:
-        A formatted string representing the processed list. Or None if field_list is None.
+def process_list(field_list: list[str], join_string: str = ", ") -> str:
     """
-    if field_list is not None:
-        # Join the items into a string
-        processed_field = ", ".join(field_list)
+    Process a list of items and return a formatted string.
 
-        # If the list is longer than the max_items_to_display, truncate and display more count
-        if len(processed_field) > 1024:
-            visible_items = processed_field[:1024].split(",")
+    :param field_list: The list of items to process.
+    :param join_string: The string to use in the join operation.
+    :return: A formatted string representing the processed list. Or None if field_list is None.
+    """
+
+    # Join the items into a string
+    processed_field = join_string.join(field_list)
+
+    # If the list is longer than the max_items_to_display, truncate and display more count
+    if len(processed_field) > 1024:
+        visible_items = processed_field[:1024].split(join_string)
+
+        cut_off_items = processed_field[1024:].split(join_string)
+        cut_off_items_length = len(cut_off_items)
+
+        if not visible_items[-1].endswith(")"):
             del visible_items[-1]
-            cut_off_items = len(processed_field[1024:].split(",")) + 1
-            visible_items_string = ", ".join(visible_items)
-            processed_field = f"{visible_items_string} and {cut_off_items} more"
 
-        return processed_field
+        visible_items_string = join_string.join(visible_items)
+        processed_field = f"{visible_items_string} and {cut_off_items_length} more"
 
-    # If field_list is None
-    return None
+    return processed_field
 
 
-def delete_first_and_last_from_list(list_arg: list[str] | None, threshold: int = 3):
-    if list_arg is not None and len(list_arg) >= threshold:
-        # Remove the first and last items from the list if the list is long enough
-        del list_arg[0]
-        del list_arg[-1]
+def trim_list_ends(list_arg: list[str], min_length: int = 3) -> list[str]:
+    """
+    Removes the first and last items from a list if the list length
+    is greater than or equal to the specified minimum length.
+
+    :param list_arg: The list to be trimmed.
+    :param min_length: The minimum length required for trimming the list. Defaults to 3.
+    :return: The trimmed list or the original list.
+    """
+    if len(list_arg) >= min_length:
+        list_arg = list_arg[1:-1]
     return list_arg
+
+
+def create_footer(embed: Embed):
+    return embed.set_footer(
+        text=f"Info retrieved by Summarium on {now.strftime('%a %d at %X')}"
+    )
+
+
+def regex_strings(site: Site):
+    match site:
+        case site.ArchiveOfOurOwn:
+            return (
+                r"^https://archiveofourown\.org/(\bseries\b|\bworks\b|\bcollections\b)/"
+            )
+        case site.FanFictionDotNet:
+            return r"^https://(www|m)\.(\bfanfiction\b\.\bnet\b)/s/\d+/\d+/\w*"
+        case site.SpaceBattles:
+            return r"^https?://forums?\.spacebattles\.com/threads/(([a-zA-Z%0-9]+-*)+\.[0-9]+)/?"
+        case site.FictionLive:
+            return r"^https?://fiction\.live/(?:stories|Sci-fi)/([^\/]+|)/([0-9a-zA-Z\-]+)/?.*"
+        case site.ScribbleHub:
+            return r"(^https://www\.scribblehub\.com/(series|read|profile))/\d+"
+        case site.WebNovel:
+            return r"(www|m)\.webnovel\.com/book/"
+        case _:
+            raise TypeError("not a point we support")
