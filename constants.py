07c08fa0f30f1ba79c991fe5801f122bb874e25c
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2024-04-29 14:52:35 +0100

    feat: Added a constants.py file

---
+++ constants.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class Site(Enum):
+    ArchiveOfOurOwn = "ArchiveOfOurOwn"
+    FanFictionDotNet = "FanFictionDotNet"
+    SpaceBattles = "SpaceBattles"
+    FictionLive = "FictionLive"
+    ScribbleHub = "ScribbleHub"
+    WebNovel = "WebNovel"
+    # QuestionableQuesting = "QuestionableQuesting"
+    # SufficientVelocity = "SufficientVelocity"
